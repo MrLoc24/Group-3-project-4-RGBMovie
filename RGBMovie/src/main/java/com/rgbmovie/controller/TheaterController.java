b2d5db3617110683eff157b87e6eb46b70fb46f8
MrLoc24 <loclongla1999@gmail.com>
2023-09-27 15:37:23 +0700

    update

--- RGBMovie/src/main/java/com/rgbmovie/controller/TheaterController.java
+++ RGBMovie/src/main/java/com/rgbmovie/controller/TheaterController.java
@@ -1,2 +1,80 @@
-package com.rgbmovie.controller;public class TheaterController {
+
+package com.rgbmovie.controller;
+
+import java.util.List;
+
+import com.rgbmovie.dto.TheaterDTO;
+import com.rgbmovie.service.TheaterService;
+import jakarta.servlet.http.HttpServletRequest;
+
+import com.rgbmovie.dto.RoleDTO;
+import com.rgbmovie.service.RoleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/theater")
+public class TheaterController {
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private TheaterService theaterService;
+
+
+    @RequestMapping(value = {""}, method = RequestMethod.GET)
+    public String index(Model model, HttpServletRequest request, RedirectAttributes redirect) {
+        request.getSession().setAttribute("theaterList", null);
+        if (model.asMap().get("success") != null)
+            redirect.addFlashAttribute("success", model.asMap().get("success").toString());
+        return "redirect:/theater/page/1";
+    }
+
+    @RequestMapping(value = "/page/{pageNumber}", method = RequestMethod.GET)
+    public String showUserPage(HttpServletRequest request,
+
+                               @PathVariable int pageNumber, Model model) {
+        PagedListHolder<?> pages = (PagedListHolder<?>) request.getSession().getAttribute("theaterList");
+        int pageSize = 10;
+        List<TheaterDTO> list = theaterService.getAll().stream().map(m -> modelMapper.map(m, TheaterDTO.class)).toList();
+        if (pages == null) {
+            pages = new PagedListHolder<>(list);
+            pages.setPageSize(pageSize);
+        } else {
+            final int goToPage = pageNumber - 1;
+            if (goToPage <= pages.getPageCount() && goToPage >= 0) {
+                pages.setPage(goToPage);
+            }
+        }
+        request.getSession().setAttribute("theaterList", pages);
+        int current = pages.getPage() + 1;
+        int begin = Math.max(1, current - list.size());
+        int end = Math.min(begin + 9, pages.getPageCount());
+        int totalPageCount = pages.getPageCount();
+        if (current >= end) {
+            current = pages.getPage() + 1;
+            begin = Math.max(current - end + 1, current - list.size());
+            end = Math.min(current, pages.getPageCount());
+            // totalPageCount = pages.getPageCount();
+        }
+        String baseUrl = "/theater/page/";
+        model.addAttribute("beginIndex", begin);
+        model.addAttribute("endIndex", end);
+        model.addAttribute("currentIndex", current);
+        model.addAttribute("totalPageCount", totalPageCount);
+        model.addAttribute("baseUrl", baseUrl);
+        model.addAttribute("theaters", pages);
+        return "theater/index";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable("id") int id) {
+        return "theater/detail";
+    }
+>>>>>>> Stashed changes
 }
